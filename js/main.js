0353e621a722b463b04eee22e6ca6d96fa254279
Miłosz <milosz.devmail@gmail.com>
2024-09-03 19:11:19 +0200

    Add filtering

--- js/main.js
+++ js/main.js
@@ -1,37 +1,46 @@
 const mainContainer = document.getElementById("mainContainer");
+const searchInput = document.getElementById("searchInput");
 const form = document.getElementById("noteForm");
 const noteList = document.querySelector(".noteList");
 const formSubmitButton = document.getElementById("noteFormSubmit");
 const formTitle = document.getElementById("noteFormTitle");
 
-let editedNoteIndex = null;
+let editedNoteId = null;
 
-const notes = [
+let notes = [
     {
+        id: getId(),
         title: "Raz dwa tzy aiosjd",
         description: "make",
         creationDate: new Date(),
     },
     {
+        id: getId(),
         title: "Another note",
         description: "It",
         creationDate: new Date(),
     },
     {
+        id: getId(),
         title: "Raz dwa tzy aiosjd",
         description: "typ",
         creationDate: new Date(),
     },
 ];
 
-const formatDate = (date) => {
+function getId() {
+    return Math.random().toString().split(".")[1].slice(0, 5);
+}
+
+function formatDate(date) {
     return new Intl.DateTimeFormat("en-US", {
         day: "2-digit",
         month: "short",
     }).format(date);
-};
+}
 
-const renderNotes = () => {
+function renderNotes(filterQuery) {
+    filterQuery = searchInput.value;
     noteList.innerHTML = "";
 
     if (!notes.length) {
@@ -50,33 +59,63 @@ const renderNotes = () => {
         return;
     }
 
-    notes.forEach((note, index) => {
+    const filteredNotes = filterQuery
+        ? notes.filter((note) => {
+              return (
+                  note.title.toLowerCase().includes(filterQuery) ||
+                  note.description.toLowerCase().includes(filterQuery)
+              );
+          })
+        : notes;
+
+    if (filterQuery && !filteredNotes.length) {
+        noteList.innerHTML = `
+        <div class="emptyState">
+            <div class="emptyState__circle">
+                <img src="/assets/info.svg" />
+            </div>
+            <div class="emptyState__title">No results</div>
+            <p class="emptyState__text">Try to change the filter.</p>
+        </div>
+    `;
+        return;
+    }
+
+    filteredNotes.forEach((note) => {
         const noteElement = document.createElement("div");
+
+        const { id, title, description } = note;
 
         const dateToRender = formatDate(note.creationDate);
 
         noteElement.className = "note";
         noteElement.innerHTML = `
             <div class="note__header">
-                <h2 class="note__title">${note.title}</h2>
+                <h2 class="note__title">${title}</h2>
                 <div class="note__buttonsContainer">
-                    <button onclick="showForm(${index})" class="buttonGhost" aria-label="Edit note" title="Edit"><img src="/assets/edit.svg" /></button>
-                    <button onclick="showConfirmDeleteModal()" class="buttonGhost" aria-label="Delete note" title="Delete"><img src="/assets/trash.svg" /></button>
+                    <button onclick="showForm(${id})" class="buttonGhost" aria-label="Edit note" title="Edit"><img src="/assets/edit.svg" /></button>
+                    <button onclick="showConfirmDeleteModal(${id})" class="buttonGhost" aria-label="Delete note" title="Delete"><img src="/assets/trash.svg" /></button>
                 </div>
             </div>
-            <p class="note__description">${note.description}</p>
+            <p class="note__description">${description}</p>
             <div class="note__date">${dateToRender}</div>
         `;
 
         noteList.appendChild(noteElement);
     });
-};
+}
 
-function showForm(index) {
-    if (typeof index === "number" && notes[index]) {
-        const { title, description } = notes[index];
+function showForm(id) {
+    id = id ? id.toString() : undefined;
 
-        editedNoteIndex = index;
+    if (id) {
+        const note = notes.find((note) => note.id === id);
+
+        if (!note) return;
+
+        const { title, description } = note;
+
+        editedNoteId = id;
 
         formSubmitButton.innerText = "Save";
         formTitle.innerText = "Edit note";
@@ -98,13 +137,14 @@ function showForm(index) {
 function hideForm() {
     form.style.display = "none";
 
-    editedNoteIndex = null;
+    editedNoteId = null;
 
     form.reset();
 }
 
-const addNote = (title, description) => {
+function addNote(title, description) {
     const newNote = {
+        id: getId(),
         title,
         description,
         creationDate: new Date(),
@@ -116,10 +156,12 @@ const addNote = (title, description) => {
 
     noteForm.reset();
     hideForm();
-};
+}
 
-const editNote = (title, description) => {
-    const note = notes[editedNoteIndex];
+function editNote(title, description) {
+    const note = notes.find((note) => note.id === editedNoteId);
+
+    if (!note) return;
 
     const newNote = {
         ...note,
@@ -127,22 +169,30 @@ const editNote = (title, description) => {
         description,
     };
 
-    notes[editedNoteIndex] = newNote;
+    notes = notes.map((note) => {
+        if (note.id === editedNoteId) {
+            return newNote;
+        }
+
+        return note;
+    });
 
     renderNotes();
 
     noteForm.reset();
     hideForm();
-};
+}
 
-function deleteNote(index) {
-    notes.splice(index, 1);
+function deleteNote(id) {
+    id = id ? id.toString() : undefined;
+
+    notes = notes.filter((note) => note.id !== id);
 
     closeConfirmDeleteModal();
     renderNotes();
 }
 
-function showConfirmDeleteModal(index) {
+function showConfirmDeleteModal(id) {
     const modal = document.createElement("div");
 
     modal.className = "modal";
@@ -154,7 +204,7 @@ function showConfirmDeleteModal(index) {
                 <div class="modal__text">Are you sure you want to delete this note?</div>
                 <div class="modal__buttonsContainer">
                     <button onclick="closeConfirmDeleteModal()" type="button" class="buttonOutline">Cancel</button>
-                    <button onclick="deleteNote(${index})" type="button" class="buttonPrimary">Delete</button>
+                    <button onclick="deleteNote(${id})" type="button" class="buttonPrimary">Delete</button>
                 </div>
             </div>
         </div>
@@ -169,19 +219,27 @@ function closeConfirmDeleteModal() {
     modal.remove();
 }
 
-const handleFormSubmit = (event) => {
+function handleFormSubmit(event) {
     event.preventDefault();
 
     const { value: title } = document.getElementById("noteTitle");
     const { value: description } = document.getElementById("noteDescription");
 
-    if (typeof editedNoteIndex === "number") {
+    if (editedNoteId) {
         editNote(title, description);
         return;
     }
 
     addNote(title, description);
-};
+}
+
+function filterNotes(event) {
+    const query = event.target.value.toLowerCase();
+
+    renderNotes(query);
+}
+
+searchInput.addEventListener("input", filterNotes);
 
 noteForm.addEventListener("submit", handleFormSubmit);
 renderNotes();
